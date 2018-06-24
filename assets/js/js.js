3546fe6986ab2e10116eeeecf7c9cf91b8340f81
dicimafran <cimafran16@gmail.com>
2018-06-24 19:01:51 -0400

    firebase stuff is working, need to fix new table row function amongst other things

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,8 +1,107 @@
-$(document).ready(function(){
-   
+
 /*  =============== Table of contents ============================
+
+    Firebase 
+        Initialize firebase
+        db stands for database
+        var db is assigned to the firebase's database
+
+    Global Variables
+        dbTrainInfo: an array which contains info which was grabbed from the firebase database
+
+    Global Functions
+        initialValues: clears all the targeted text boxes
+        
+    Calls and Events
+        1) document.ready
+        2) captures input on click of the submit button
+        3) 
+
+    Pseudocoding
     
-Psueudocoding
+    ---------- WIP stuff & other comments -----------------------
+        
+            train frequency input: 
+                This needs limits to only accept numbers in a certain format.
+            
+            table row function: 
+                
+    ============================ Firebase =======================================*/
+
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC7yg5tMoDDqC_NfbUxRNGsovOHZSi2_2w",
+    authDomain: "train-e9810.firebaseapp.com",
+    databaseURL: "https://train-e9810.firebaseio.com",
+    projectId: "train-e9810",
+    storageBucket: "train-e9810.appspot.com",
+    messagingSenderId: "376706306645"
+  };
+  
+  firebase.initializeApp(config);
+  var db = firebase.database();
+
+// ====================== Global Variables ===============================
+
+    var dbTrainInfo= [];
+
+// ====================== Global functions ===============================
+ function initialValues () {
+    var name  = $('#trainName').val('');
+    var destination = $('#destination').val('');
+    var firstTrainTime = $('#firstTrainTime').val('');
+    var freq = $('#trainFrequency').val('');
+}
+
+// I want to create a function which takes info from dbTrainInfo array, creates rows for each item, and then appends it to the table on the DOM
+function newTableRow (dbTrainInfo) {
+     for (i=0; i < dbTrainInfo.length; i++){
+         $('#trainTable > tbody').append(
+             '<tr><td>' + dbTrainInfo.trainName [i] + '</td></tr>'  +
+             '<tr><td>' + dbTrainInfo.destination [i] + '</td></tr>' +
+             '<tr><td>'+ dbTrainInfo.trainTime [i] + '</td></tr>'
+         )
+     }
+}
+
+// ======================= Calls and Events ==================
+
+$(document).ready(function(){
+
+  // Function to add inputs on click
+  $('#addTrain').click(function(event) {
+    event.preventDefault();
+
+    // grabs user input
+    let name  = $('#trainName').val().trim();
+    let destination = $('#destination').val().trim();
+    let firstTrainTime = $('#firstTrainTime').val().trim();
+    let freq = $('#trainFrequency').val().trim()
+
+    // creates local temporary object to store new input
+    var newTrainInfo = {
+        trainName: name,
+        destination: destination,
+        trainTime: firstTrainTime,
+    };
+        
+    // pushes newTrainInfo object into the database 
+        db.ref().push(newTrainInfo);
+        console.log (newTrainInfo);
+    
+    // clears the input text boxes
+    initialValues();    
+  });
+
+  // Function to create firebase event to add train to database
+  db.ref().on('child_added', function(childSnapshot, prevChildKey) {
+        dbTrainInfo.push(childSnapshot.val());
+  })  
+}); 		
+
+console.log(dbTrainInfo);
+
+/*========================= Psueudocoding ==============================
     Inputs: Name of train, destination, first train, frequency (min)
         grab user input and store as var
         create local object to hold data
@@ -10,52 +109,4 @@ Psueudocoding
 
     Outputs: Train name, destination, frequency (min), next arrival, minutes away
     Process: firebase
-
-Abbreviations
-    db= database
-
-============================ Firebase =======================================*/
-/*
- // Initialize Firebase
- var config = {
-    apiKey: "AIzaSyC7yg5tMoDDqC_NfbUxRNGsovOHZSi2_2w",
-    authDomain: "train-e9810.firebaseapp.com",
-    databaseURL: "https://train-e9810.firebaseio.com",
-    projectId: "train-e9810",
-    storageBucket: "",
-    messagingSenderId: "376706306645"
-  };
-  firebase.initializeApp(config);
-
-  var db = firebase.database;
-*/
-  // Function to add inputs on click
-  $('#addTrain').click(function(event) {
-    event.preventDefault();
-
-    // grabs user input
-    var name  = $('#trainName').val().trim();
-    var destination = $('#destination').val().trim();
-    var firstTrainTime = $('#firstTrainTime').val().trim();
-    var freq = $('#trainFrequency').val().trim()
-
-    // making temporary object to store input
-    var trainTemp = {
-        trainName: name,
-        destination: destination,
-        trainTime: firstTrainTime,
-    };
-
-    console.log (trainTemp);
-
-    var trainInfo = [];
-    
-
-  })
-
-
-
-
-
-
-}); 		
+ */
