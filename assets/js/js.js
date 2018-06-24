f5ea05e10e36be6f3447afc21dff9c58ddfff69a
dicimafran <cimafran16@gmail.com>
2018-06-23 23:44:24 -0400

    layout done, working on js

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,15 +1,57 @@
 $(document).ready(function(){
    
-/* 
-Inputs: Name of train, destination, first train, frequency (min)
-Outputs: Train name, destination, frequency (min), next arrival, minutes away
-Process: 
+/*  =============== Table of contents ============================
+    
+Psueudocoding
+    Inputs: Name of train, destination, first train, frequency (min)
+        grab user input and store as var
+        create local object to hold data
+        upload the data to the database
+
+    Outputs: Train name, destination, frequency (min), next arrival, minutes away
+    Process: firebase
+
+Abbreviations
+    db= database
+
+============================ Firebase =======================================*/
+/*
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyC7yg5tMoDDqC_NfbUxRNGsovOHZSi2_2w",
+    authDomain: "train-e9810.firebaseapp.com",
+    databaseURL: "https://train-e9810.firebaseio.com",
+    projectId: "train-e9810",
+    storageBucket: "",
+    messagingSenderId: "376706306645"
+  };
+  firebase.initializeApp(config);
+
+  var db = firebase.database;
 */
+  // Function to add inputs on click
+  $('#addTrain').click(function(event) {
+    event.preventDefault();
 
-$(document).click(function () {
-    console.log("whale")
-})
+    // grabs user input
+    var name  = $('#trainName').val().trim();
+    var destination = $('#destination').val().trim();
+    var firstTrainTime = $('#firstTrainTime').val().trim();
+    var freq = $('#trainFrequency').val().trim()
 
+    // making temporary object to store input
+    var trainTemp = {
+        trainName: name,
+        destination: destination,
+        trainTime: firstTrainTime,
+    };
+
+    console.log (trainTemp);
+
+    var trainInfo = [];
+    
+
+  })
 
 
 
