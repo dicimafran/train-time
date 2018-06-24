f83f3bf45b208f4630547cc0e69e42072ec4a156
dicimafran <cimafran16@gmail.com>
2018-06-23 21:56:51 -0400

    layout mostly done

---
+++ assets/js/js.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+   
+/* 
+Inputs: Name of train, destination, first train, frequency (min)
+Outputs: Train name, destination, frequency (min), next arrival, minutes away
+Process: 
+*/
+
+$(document).click(function () {
+    console.log("whale")
+})
+
+
+
+
+
+
+
+}); 		
